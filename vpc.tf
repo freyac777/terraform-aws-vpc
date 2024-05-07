be35971b560829616f71f94c996c8c9503de6701
freyac777 <164959620+freyac777@users.noreply.github.com>
2024-05-06 20:44:57 -0500

    az

--- vpc.tf
+++ vpc.tf
@@ -5,7 +5,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = var.region
+  availability_zone = "us-east-2a"
 
   tags = {
     Name = "public-subnet"
@@ -16,7 +16,7 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = var.region
+  availability_zone = "us-east-2a"
 
 
   tags = {
